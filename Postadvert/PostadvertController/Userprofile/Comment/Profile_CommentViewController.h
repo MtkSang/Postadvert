2dee0904646aff36f80598994d4f1ef495d79a1d
MtkSang <sang@futureworkz.com>
2012-11-06 11:44:55 +0700

    newCommit
    Add ProfileCommentViewController

---
+++ Postadvert/PostadvertController/Userprofile/Comment/Profile_CommentViewController.h
@@ -0,0 +1,13 @@
+//
+//  Profile_CommentViewController.h
+//  Stroff
+//
+//  Created by Nguyen Ngoc Sang on 11/6/12.
+//  Copyright (c) 2012 Futureworkz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Profile_CommentViewController : UIViewController
+
+@end
---
+++ Postadvert/PostadvertController/Userprofile/Comment/Profile_CommentViewController.m
@@ -0,0 +1,38 @@
+//
+//  Profile_CommentViewController.m
+//  Stroff
+//
+//  Created by Nguyen Ngoc Sang on 11/6/12.
+//  Copyright (c) 2012 Futureworkz. All rights reserved.
+//
+
+#import "Profile_CommentViewController.h"
+
+@interface Profile_CommentViewController ()
+
+@end
+
+@implementation Profile_CommentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
