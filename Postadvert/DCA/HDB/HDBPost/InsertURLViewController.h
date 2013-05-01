792a4abf70b6e34aa5c6136bee43598212267392
MtkSang <sang@futureworkz.com>
2013-05-01 16:41:04 +0700

    010513
    ALL

---
+++ Postadvert/DCA/HDB/HDBPost/InsertURLViewController.h
@@ -0,0 +1,13 @@
+//
+//  InsertURLViewController.h
+//  Stroff
+//
+//  Created by Ray on 4/18/13.
+//  Copyright (c) 2013 Futureworkz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InsertURLViewController : UIViewController
+
+@end
---
+++ Postadvert/DCA/HDB/HDBPost/InsertURLViewController.m
@@ -0,0 +1,38 @@
+//
+//  InsertURLViewController.m
+//  Stroff
+//
+//  Created by Ray on 4/18/13.
+//  Copyright (c) 2013 Futureworkz. All rights reserved.
+//
+
+#import "InsertURLViewController.h"
+
+@interface InsertURLViewController ()
+
+@end
+
+@implementation InsertURLViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
